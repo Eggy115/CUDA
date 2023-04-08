21740c8ca1dddb0a81c1dfb44fe928250f430f42
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-04-08 09:39:48 +0100

    Add files via upload

--- dot.cu
+++ dot.cu
@@ -15,24 +15,16 @@
 
 
 #include "../common/book.h"
-#include "lock.h"
-
-
-// this example is the dot from a previous chapter, modified so that it
-// doesn't have to do the final step on the CPU.  Each grid block has
-// to add its float value to the global total, but since we do not have
-// an atomicAddFloat, we can do it this way
-
 
 #define imin(a,b) (a<b?a:b)
 
-const int N = 33 * 1024 * 1024;
+const int N = 33 * 1024;
 const int threadsPerBlock = 256;
 const int blocksPerGrid =
             imin( 32, (N+threadsPerBlock-1) / threadsPerBlock );
 
-__global__ void dot( Lock lock, float *a,
-                     float *b, float *c ) {
+
+__global__ void dot( float *a, float *b, float *c ) {
     __shared__ float cache[threadsPerBlock];
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
     int cacheIndex = threadIdx.x;
@@ -59,31 +51,27 @@ __global__ void dot( Lock lock, float *a,
         i /= 2;
     }
 
-    if (cacheIndex == 0) {
-        // wait until we get the lock
-        lock.lock();
-       // we have the lock at this point, update and release
-        *c += cache[0];
-        lock.unlock();
-    }
+    if (cacheIndex == 0)
+        c[blockIdx.x] = cache[0];
 }
 
 
 int main( void ) {
-    float   *a, *b, c = 0;
-    float   *dev_a, *dev_b, *dev_c;
+    float   *a, *b, c, *partial_c;
+    float   *dev_a, *dev_b, *dev_partial_c;
 
     // allocate memory on the cpu side
     a = (float*)malloc( N*sizeof(float) );
     b = (float*)malloc( N*sizeof(float) );
+    partial_c = (float*)malloc( blocksPerGrid*sizeof(float) );
 
     // allocate the memory on the GPU
     HANDLE_ERROR( cudaMalloc( (void**)&dev_a,
                               N*sizeof(float) ) );
     HANDLE_ERROR( cudaMalloc( (void**)&dev_b,
                               N*sizeof(float) ) );
-    HANDLE_ERROR( cudaMalloc( (void**)&dev_c,
-                              sizeof(float) ) );
+    HANDLE_ERROR( cudaMalloc( (void**)&dev_partial_c,
+                              blocksPerGrid*sizeof(float) ) );
 
     // fill in the host memory with data
     for (int i=0; i<N; i++) {
@@ -96,17 +84,20 @@ int main( void ) {
                               cudaMemcpyHostToDevice ) );
     HANDLE_ERROR( cudaMemcpy( dev_b, b, N*sizeof(float),
                               cudaMemcpyHostToDevice ) ); 
-    HANDLE_ERROR( cudaMemcpy( dev_c, &c, sizeof(float),
-                              cudaMemcpyHostToDevice ) ); 
 
-    Lock    lock;
-    dot<<<blocksPerGrid,threadsPerBlock>>>( lock, dev_a,
-                                            dev_b, dev_c );
+    dot<<<blocksPerGrid,threadsPerBlock>>>( dev_a, dev_b,
+                                            dev_partial_c );
 
-    // copy c back from the GPU to the CPU
-    HANDLE_ERROR( cudaMemcpy( &c, dev_c,
-                              sizeof(float),
+    // copy the array 'c' back from the GPU to the CPU
+    HANDLE_ERROR( cudaMemcpy( partial_c, dev_partial_c,
+                              blocksPerGrid*sizeof(float),
                               cudaMemcpyDeviceToHost ) );
+
+    // finish up on the CPU side
+    c = 0;
+    for (int i=0; i<blocksPerGrid; i++) {
+        c += partial_c[i];
+    }
 
     #define sum_squares(x)  (x*(x+1)*(2*x+1)/6)
     printf( "Does GPU value %.6g = %.6g?\n", c,
@@ -115,9 +106,10 @@ int main( void ) {
     // free memory on the gpu side
     HANDLE_ERROR( cudaFree( dev_a ) );
     HANDLE_ERROR( cudaFree( dev_b ) );
-    HANDLE_ERROR( cudaFree( dev_c ) );
+    HANDLE_ERROR( cudaFree( dev_partial_c ) );
 
     // free memory on the cpu side
     free( a );
     free( b );
+    free( partial_c );
 }
